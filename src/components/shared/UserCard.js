a2e7c5504c3e5fdf171213f6c6a2949f443b7a8c
Paola Garay <garay.angela@gmail.com>
2021-02-15 10:45:00 -0500

    Add UserCard component structure

--- src/components/shared/UserCard.js
+++ src/components/shared/UserCard.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Avatar, Typography } from "@material-ui/core";
+
+import { defaultUser } from "../../data";
 import { useUserCardStyles } from "../../styles";
 
-function UserCard() {
-  useUserCardStyles();
 
-  return <div>UserCard</div>;
+function UserCard({ user = defaultUser, avatarSize = 44 }) {
+  const classes = useUserCardStyles({ avatarSize });
+  const { username, name, profile_image } = user;
+
+  return (
+    <div className={classes.wrapper}>
+      <Link to={`/${username}`}>
+        <Avatar
+          src={profile_image}
+          alt="User avatar"
+          className={classes.avatar}
+        />
+      </Link>
+      <div className={classes.nameWrapper}>
+        <Link to={`/${username}`}>
+          <Typography variant="subtitle2" className={classes.typography}>
+            {username}
+          </Typography>
+        </Link>
+        <Typography
+          color="textSecondary"
+          variant="body2"
+          className={classes.typography}
+        >
+          {name}
+        </Typography>
+      </div>
+    </div>
+  );
 }
 
 export default UserCard;
